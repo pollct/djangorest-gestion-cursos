38f5481e325afad650edc8bfebb2de1e46af2b45
pollct <poll.cedeno@outlook.com>
2024-12-13 21:29:51 -0500

    First Commit

---
+++ courses/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    courses = models.ManyToManyField(Course, related_name='students')
+
+    def __str__(self):
+        return self.name
